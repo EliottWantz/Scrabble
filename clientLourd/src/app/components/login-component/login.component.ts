707897c62533f8b4d122296521d78bfbdae7e437
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-01-29 19:15:14 -0500

    Ajout de connect et créer compte pour login component (sans communication avec le serveur)

--- clientLourd/src/app/components/login-component/login.component.ts
+++ clientLourd/src/app/components/login-component/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { MatDialog } from '@angular/material/dialog';
+import { AuthentificationService } from "@app/services/authentification/authentification.service";
 
 export const SMALLEST = -1;
 
@@ -9,23 +10,44 @@ export const SMALLEST = -1;
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
-    constructor(private matDialog: MatDialog) {
-
-    }
-  username: string = "";
-  password: string = "";
-  show: boolean = false;
-  submit() {
+  hasAccount: Boolean;
+  username: string;
+  password: string;
+  email: string;
+  show: boolean;
+  constructor(private matDialog: MatDialog, private authService: AuthentificationService) {
+    this.hasAccount = true;
+    this.username = "";
+    this.password = "";
+    this.email = "";
+    this.show = false;
+  }
+  
+  createAccount() {
     console.log("user name is " + this.username);
     this.clear();
   }
+
+  connect() {
+    const succeeded: Boolean = this.authService.login(this.username, this.password);
+    if (succeeded) {
+      console.log(this.authService.user);
+    }
+    console.log("user name is " + this.username);
+    this.clear();
+  }
+
   clear() {
     this.username = "";
     this.password = "";
     this.show = true;
   }
 
+  switchView(): void {
+    this.hasAccount = !this.hasAccount;
+  }
+
   closeModal(): void {
     this.matDialog.closeAll();
-}
+  }
 }
