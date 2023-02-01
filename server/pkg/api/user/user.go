69564eb622463bc4468dd719587cd3fcaa977793
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2023-02-01 17:40:39 -0500

    fix: removed password in http response

--- server/pkg/api/user/user.go
+++ server/pkg/api/user/user.go
@@ -3,7 +3,7 @@ package user
 type User struct {
 	Id             string `bson:"_id,omitempty" json:"id,omitempty"`
 	Username       string `bson:"username" json:"username,omitempty"`
-	HashedPassword string `bson:"password" json:"-,omitempty"`
+	HashedPassword string `bson:"password" json:"-"`
 	Email          string `bson:"email" json:"email,omitempty"`
 	AvatarURL      string `bson:"avatarUrl" json:"avatarUrl,omitempty"`
 	Preferences    Preferences
