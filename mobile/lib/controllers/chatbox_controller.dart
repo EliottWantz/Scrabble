74fc94ecf7d929491cea08f4b53a515ccbbc7ea8
ivanivanov-gitlab <ivan.ivanov@polymtl.ca>
2023-02-06 18:59:19 -0500

    added chatbox controller with send message function

---
+++ mobile/lib/controllers/chatbox_controller.dart
@@ -0,0 +1,17 @@
+import 'package:client_leger/services/websocket_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class ChatBoxController extends GetxController {
+  final WebsocketService websocketService;
+
+  ChatBoxController({required this.websocketService});
+
+  TextEditingController textController = TextEditingController();
+
+  void sendMessage() {
+    if (textController.text.isNotEmpty) {
+      websocketService.sendMessage(textController.text);
+    }
+  }
+}
