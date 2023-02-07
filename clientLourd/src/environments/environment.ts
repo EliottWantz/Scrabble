d41ba43ce90760be7ca3243ae8fda551d76dbd36
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-02-07 15:20:14 -0500

    changement ws dans environnement

--- clientLourd/src/environments/environment.ts
+++ clientLourd/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     serverUrl: 'http://127.0.0.1:3000/api',
-    wsUrl: 'ws://localhost:3000/',
+    wsUrl: 'http://localhost:3000/ws',
 };
 
 /*
