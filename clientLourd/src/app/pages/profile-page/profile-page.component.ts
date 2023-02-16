21d43d287e2f83faf0c9b1e6537ce82b6ce81033
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-02-16 16:31:37 -0500

    Création de la page de profile

---
+++ clientLourd/src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+//import { MatDialog } from "@angular/material/dialog";
+//import { AuthentificationService } from "@app/services/authentification/authentification.service";
+//import { LoginComponent } from "@app/components/login-component/login.component";
+
+@Component({
+  selector: "app-profile-page",
+  templateUrl: "./profile-page.component.html",
+  styleUrls: ["./profile-page.component.scss"],
+})
+export class ProfilePageComponent {
+  constructor() {
+    console.log("hello");
+  }
+  ngOnInit(): void {
+    
+  }
+}
