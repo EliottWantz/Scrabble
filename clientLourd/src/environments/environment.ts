162c6734ca7cada706ee43ea6e33a29c9fa87f31
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-02-16 16:32:06 -0500

    Modification du lien pour le websocket

--- clientLourd/src/environments/environment.ts
+++ clientLourd/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  // serverUrl: 'http://127.0.0.1:3000/api',
-  // wsUrl: 'http://localhost:3000/ws',
-  serverUrl: "https://scrabble-production.up.railway.app/api", // TODO: Add remote server URL
-  wsUrl: "wss://scrabble-production.up.railway.app/ws",
+  serverUrl: 'http://127.0.0.1:3000/api',
+  wsUrl: 'ws://localhost:3000/ws',
+  //serverUrl: "https://scrabble-production.up.railway.app/api", // TODO: Add remote server URL
+  //wsUrl: "wss://scrabble-production.up.railway.app/ws",
 };
 
 /*
