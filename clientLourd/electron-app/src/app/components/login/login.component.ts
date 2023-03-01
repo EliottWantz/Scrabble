c155929dc6cdb1e87235ba62d7874b6a1eb0b7f6
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-01 18:03:29 -0500

    Clean up de code

--- clientLourd/electron-app/src/app/components/login/login.component.ts
+++ clientLourd/electron-app/src/app/components/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-//import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthenticationService } from "@app/services/authentication/authentication.service";
 import { Router } from "@angular/router"
 
