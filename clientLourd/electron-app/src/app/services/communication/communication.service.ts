4eeb63e91c544bd60faa2b93f4b2414b890b6435
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-01 15:04:11 -0500

    Ajout de error handling dan comm service

--- clientLourd/electron-app/src/app/services/communication/communication.service.ts
+++ clientLourd/electron-app/src/app/services/communication/communication.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from 'src/environments/environment';
 import { User } from "@app/utils/interfaces/user";
-import { catchError, Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 
 @Injectable({
     providedIn: 'root',
@@ -13,10 +13,24 @@ export class CommunicationService {
     constructor(private readonly http: HttpClient) {}
 
     login(username: string, password: string): Observable<{user: User}> {
-        return this.http.post<{user: User}>(`${this.baseUrl}/login`, { username, password });
+        return this.http.post<{user: User}>(`${this.baseUrl}/login`, { username, password }).pipe(catchError(this.handleError));
     }
 
     register(username: string, password: string, email: string, avatar: string): Observable<{user: User}> {
-        return this.http.post<{user: User}>(`${this.baseUrl}/signup`, { username, password, email, avatar });
+        return this.http.post<{user: User}>(`${this.baseUrl}/signup`, { username, password, email, avatar }).pipe(catchError(this.handleError));
     }
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.status === 0) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.error('An error occurred:', error.error);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong.
+          console.error(
+            `Backend returned code ${error.status}, body was: `, error.error);
+        }
+        // Return an observable with a user-facing error message.
+        return throwError(() => new Error('Something bad happened; please try again later.'));
+      }
 }
