29a2a38302f15f28eaf31ecd01754e919ec55e6e
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-04 19:23:38 -0500

    Modifications pour utiliser l'architecture

--- clientLourd/electron-app/src/app/components/chat-box/chat-box-prototype.component.ts
+++ clientLourd/electron-app/src/app/components/chat-box/chat-box-prototype.component.ts
@@ -12,15 +12,11 @@ import {
   Validators,
 } from "@angular/forms";
 import { MessageErrorStateMatcher } from "@app/classes/form-error/error-state-form";
-import { AuthenticationService } from "@app/services/authentication/authentication.service";
-import { BehaviorSubject } from "rxjs";
-import {
-  BroadcastPayload,
-  ChatMessage,
-  JoinedGlobalRoomPayload,
-  Packet,
-  User,
-} from "./Exports";
+import { ChatService } from "@app/services/chat/chat.service";
+import { BehaviorSubject, Subscription } from "rxjs";
+import { ChatMessage } from "@app/utils/interfaces/chat-message";
+import { User } from "@app/utils/interfaces/user";
+import { RoomService } from "@app/services/room/room.service";
 
 @Component({
   selector: "app-chat-box-prototype",
@@ -36,23 +32,28 @@ export class ChatBoxPrototypeComponent implements OnInit, AfterViewInit {
   //   messages: ChatMessage[];
   messages$!: BehaviorSubject<ChatMessage[]>;
   messageValidator: MessageErrorStateMatcher = new MessageErrorStateMatcher;
-  globalRoomId!: string;
   ws!: WebSocket;
   user!: User;
   @ViewChild("chatBoxInput")
   chatBoxInput!: ElementRef;
+  messagesSub: Subscription;
 
   constructor(
-    private authenticationService: AuthenticationService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private chatService: ChatService,
+    private roomService: RoomService
   ) {
     this.chatBoxForm = this.fb.group({
       input: ["", [Validators.required]],
     });
+    this.messagesSub = new Subscription();
   }
 
   ngOnInit(): void {
-    this.connect();
+    this.messages$ = this.chatService.messages$;
+    this.messagesSub = this.messages$.subscribe(() => {
+      setTimeout(() => this.scrollBottom());
+    });
   }
 
   ngAfterViewInit(): void {
@@ -78,59 +79,10 @@ export class ChatBoxPrototypeComponent implements OnInit, AfterViewInit {
     });
   }
 
-  async connect(): Promise<void> {
-    // this.messages = [];
-    this.messages$ = new BehaviorSubject<ChatMessage[]>([]);
-    this.user = this.authenticationService.currentUserValue;
-    this.ws = new WebSocket(
-      "wss://scrabble-production.up.railway.app/ws?id=" + this.user.id
-    );
-    this.ws.onmessage = (e) => {
-      const packet: Packet = JSON.parse(e.data);
-      switch (packet.event) {
-        case "joinedGlobalRoom":
-          this.globalRoomId = (
-            packet.payload as JoinedGlobalRoomPayload
-          ).roomId;
-          break;
-        case "broadcast": {
-          const payload = packet.payload as BroadcastPayload;
-          console.log(payload);
-          const message: ChatMessage = {
-            from: payload.from,
-            message: payload.message,
-            timestamp: new Date(payload.timestamp!).toLocaleTimeString(
-              undefined,
-              { hour12: false }
-            ),
-          };
-          // console.log(this.messages$.value);
-          this.messages$.next([...this.messages$.value, message]);
-          this.scrollBottom();
-          setTimeout(() => {
-            this.scrollBottom();
-          });
-        }
-      }
-    };
-    this.chatBoxForm.reset();
-  }
   async send(msg: string): Promise<void> {
     if (!msg || !msg.replace(/\s/g, '')) return;
-    // console.log("Sending message: " + msg);
-    const payload: BroadcastPayload = {
-      roomId: this.globalRoomId,
-      from: this.user.username,
-      message: msg,
-    };
-    console.log(this.user);
-    const packet: Packet = {
-      event: "broadcast",
-      payload: payload,
-    };
-    this.ws.send(JSON.stringify(packet));
-    // msg = "";
-    // this.chatBoxForm.setValue({ input: "" });
+
+    await this.chatService.send(msg, this.roomService.currentRoom.value);
     this.chatBoxForm.reset();
     this.chatBoxInput.nativeElement.focus();
   }
