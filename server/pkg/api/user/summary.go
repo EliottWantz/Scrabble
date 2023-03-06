d8261773e1ca613c96ce3d85455547a9521c5655
Jamesley Joseph <jamesley.joseph@polymtl.ca>
2023-03-06 00:29:25 -0500

    add Service fonction in summary.go

--- server/pkg/api/user/summary.go
+++ server/pkg/api/user/summary.go
@@ -8,7 +8,7 @@ type Summary struct {
 
 type NetworkLog struct {
 	EventType string `bson:"eventType" json:"eventType,omitempty"`
-	EvenTime  string `bson:"evenTime" json:"evenTime,omitempty"`
+	EvenTime  int    `bson:"evenTime" json:"evenTime,omitempty"`
 }
 type GameStats struct {
 	EventDate int  `bson:"eventDate" json:"eventDate,omitempty"`
@@ -20,4 +20,34 @@ type UserStats struct {
 	NbGamesWon           int `bson:"nbGamesWon" json:"nbGamesWon,omitempty"`
 	AveragePointsPerGame int `bson:"averagePointsPerGame" json:"averagePointsPerGame,omitempty"`
 	AverageTimePlayed    int `bson:"averageTimePlayed" json:"averageTimePlayed,omitempty"`
+}
+
+func (s *Service) addNetworkingLog(u *User, eventType string, eventTime int) {
+	networkLogs := &u.Summary.NetworkLogs
+	*networkLogs = append(*networkLogs, NetworkLog{
+		EventType: eventType,
+		EvenTime:  eventTime,
+	})
+	s.Repo.Update(u)
+}
+
+func (s *Service) addGameStats(u *User, eventDate int, gameWon bool) {
+	gamesStats := &u.Summary.GamesStats
+	*gamesStats = append(*gamesStats, GameStats{
+		EventDate: eventDate,
+		GameWon:   gameWon,
+	})
+	s.Repo.Update(u)
+
+}
+
+func (s *Service) updateUserStats(u *User, gameWon bool, points int, timePlayed int) {
+	userStats := &u.Summary.UserStats
+	userStats.NbGamesPlayed++
+	if gameWon {
+		userStats.NbGamesWon++
+	}
+	userStats.AveragePointsPerGame = (userStats.AveragePointsPerGame + points) / userStats.NbGamesPlayed
+	userStats.AverageTimePlayed = (userStats.AverageTimePlayed + timePlayed) / userStats.NbGamesPlayed
+	s.Repo.Update(u)
 }
