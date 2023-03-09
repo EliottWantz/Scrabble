d1d0f0d09479b54d10e47fdc097fb094731005cc
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-09 15:38:02 -0500

    Modifications pour avoir un board qui apparait

--- clientLourd/electron-app/src/app/components/board/board.component.ts
+++ clientLourd/electron-app/src/app/components/board/board.component.ts
@@ -1,14 +1,24 @@
-import { Component, HostListener } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter} from '@angular/cdk/drag-drop';
 import { Square } from "@app/utils/interfaces/square";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { Game } from "@app/utils/interfaces/game/game";
+import { GameService } from "@app/services/game/game.service";
 
 @Component({
     selector: "app-board",
     templateUrl: "./board.component.html",
     styleUrls: ["./board.component.scss"],
 })
-export class BoardComponent {
-    constructor() {
+export class BoardComponent implements OnInit {
+    game!: BehaviorSubject<Game>;
+    constructor(private gameService: GameService) {
+        this.game = this.gameService.game;
+    }
+
+    ngOnInit(): void {
+        this.game.subscribe(() => {
+            console.log("game updated");
+        });
     }
 }
--- clientLourd/electron-app/src/app/services/game/game.service.ts
+++ clientLourd/electron-app/src/app/services/game/game.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from "@angular/core";
+import { BoardHelper } from "@app/classes/board-helper";
 import { Game } from "@app/utils/interfaces/game/game";
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameService {
-    game!: Subject<Game>;
+    game!: BehaviorSubject<Game>;
     constructor() {
-        this.game = new Subject<Game>();
+        this.game = new BehaviorSubject<Game>({
+            id: "",
+            players: [],
+            board: BoardHelper.createBoard(),
+            bag: [],
+            finished: false,
+            numPassMoves: 0,
+            turn: ""
+        });
     }
 
     updateGame(game: Game): void {
