a52c309aa00a14e5c3d2947cede570359253e4a9
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-12 18:47:22 -0400

    AJout d'attribut pour l'interface user

--- clientLourd/electron-app/src/app/utils/interfaces/user.d.ts
+++ clientLourd/electron-app/src/app/utils/interfaces/user.d.ts
@@ -1,7 +1,13 @@
+import { Summary } from "@app/utils/interfaces/summary";
+
 export interface User {
     id: string;
     username: string;
     email: string;
     avatar: {url: string, fileId: string};
     preferences: {theme: string};
+    joinedChatRooms: string[];
+	friends: string[];
+	pendingRequests: string[];
+	summary: Summary;
 }
