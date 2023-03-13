64126a773e5c7bdccedd1cb5bb86c0d86cada172
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-13 16:43:30 -0400

    Ajout de la vérification si est dans game et connecté pour le bouton game

--- clientLourd/electron-app/src/app/pages/main-page/main-page.component.ts
+++ clientLourd/electron-app/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { RoomService } from "@app/services/room/room.service";
 import { UserService } from "@app/services/user/user.service";
 import { WebSocketService } from "@app/services/web-socket/web-socket.service";
 import { User } from "@app/utils/interfaces/user";
@@ -14,7 +15,7 @@ export class MainPageComponent {
   isJoining: boolean = false;
   public user: BehaviorSubject<User>;
 
-  constructor(private userService: UserService, private socketService: WebSocketService) {
+  constructor(private userService: UserService, private socketService: WebSocketService, private roomService: RoomService) {
     this.user = this.userService.subjectUser;
     document.getElementById("avatar")?.setAttribute("src", this.user.value.avatar.url);
   }
@@ -25,5 +26,9 @@ export class MainPageComponent {
 
   logout(): void {
     this.socketService.disconnect();
+  }
+
+  isInGame(): boolean {
+    return this.roomService.currentGameRoom.value.id != "";
   }
 }
