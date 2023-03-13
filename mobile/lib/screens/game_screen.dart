d82d7bef187566c7e32b803402551acdd5ab4a51
Crimo <abdelkrim.nahi@polymtl.ca>
2023-03-13 18:02:54 -0400

    Changes

--- mobile/lib/screens/game_screen.dart
+++ mobile/lib/screens/game_screen.dart
@@ -1,34 +1,15 @@
+import 'package:client_leger/services/game_service.dart';
 import 'package:client_leger/widgets/board.dart';
 import 'package:client_leger/widgets/board_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 
 final easel = List<String>.generate(7, (index) => 'A', growable: false);
 
-List<TileInfo> getTiles() {
-  const tileLayout = '''
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  F  L  U  T  T  E  R  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-    .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
-  ''';
-  return parseTiles(tileLayout).toList();
-}
-
 
 class GameScreen extends StatelessWidget {
-  const GameScreen({Key? key}) : super(key: key);
+  GameScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +21,7 @@ class GameScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const Gap(20),
-              ScrabbleBoard(tiles: getTiles()),
+              ScrabbleBoard(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: easel.map((e) => _buildEasel(e)).toList(),
