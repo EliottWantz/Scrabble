f4cbea5f7f5f25df20b78c6a543edbade14bba12
ivanivanov-gitlab <ivan.ivanov@polymtl.ca>
2023-03-13 18:49:00 -0400

    added get player method in game service

--- mobile/lib/services/game_service.dart
+++ mobile/lib/services/game_service.dart
@@ -1,11 +1,15 @@
 import 'package:client_leger/models/game_room.dart';
 import 'package:client_leger/models/user.dart';
+import 'package:client_leger/services/user_service.dart';
 import 'package:get/get.dart';
 
 import '../models/game_update_payload.dart';
+import '../models/player.dart';
 import '../models/room.dart';
 
 class GameService extends GetxService {
+  final UserService userService = Get.find();
+
   final joinableGames = Rxn<List<GameRoom>>();
 
   final currentGameRoom = Rxn<Room>();
@@ -13,5 +17,11 @@ class GameService extends GetxService {
   final currentGameRoomUsers = <User>[].obs;
 
   final currentGame = Rxn<GameUpdatePayload>();
+
+  Player? getPlayer() {
+    for (final player in currentGame.value!.players) {
+      if (player.id == userService.user.value!.id) return player;
+    }
+  }
   // final currentGameTimer = Rxn<
 }
