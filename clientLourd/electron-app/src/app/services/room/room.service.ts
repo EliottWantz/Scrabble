398fc6df367265fb246fecba1478a910c15a6e70
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-13 16:34:38 -0400

    Ajout de currentGameRoom

--- clientLourd/electron-app/src/app/services/room/room.service.ts
+++ clientLourd/electron-app/src/app/services/room/room.service.ts
@@ -11,18 +11,26 @@ import { User } from "@app/utils/interfaces/user";
 })
 export class RoomService {
     rooms!: BehaviorSubject<Room[]>;
-    currentRoom!: BehaviorSubject<Room>;
+    currentRoomChat!: BehaviorSubject<Room>;
     joinableGames!: BehaviorSubject<Room[]>;
     listChatRooms!: BehaviorSubject<Room[]>;
+    currentGameRoom!: BehaviorSubject<Room>;
 
     constructor(private commService: CommunicationService, private userService: UserService) {
         this.rooms = new BehaviorSubject<Room[]>([]);
-        this.currentRoom = new BehaviorSubject<Room>({
+        this.currentRoomChat = new BehaviorSubject<Room>({
             id: "",
             userIds: [],
             messages: [],
             creatorId : "",
             isGameRoom: false
+        });
+        this.currentGameRoom = new BehaviorSubject<Room>({
+            id: "",
+            userIds: [],
+            messages: [],
+            creatorId : "",
+            isGameRoom: true
         });
         this.joinableGames = new BehaviorSubject<Room[]>([]);
         this.listChatRooms = new BehaviorSubject<Room[]>([]);
@@ -36,14 +44,11 @@ export class RoomService {
         console.log("room");
         console.log(room);
         this.rooms.next([...this.rooms.value, room]);
-        this.currentRoom.next(room);
-    }
-
-    addJoinableGame(room: Room): void {
-        console.log(room);
-        if (room.isGameRoom)
-            this.joinableGames.next([...this.rooms.value, room]);
-        console.log(this.joinableGames.value);
+        if (room.isGameRoom) {
+            this.currentGameRoom.next(room);
+        } else {
+            this.currentRoomChat.next(room);
+        }
     }
 
     removeRoom(roomID: string): void {
@@ -58,23 +63,26 @@ export class RoomService {
     changeRoom(roomId: string): void {
         const index = this.findRoom(roomId);
         if (index !== undefined)
-            this.currentRoom.next(this.rooms.value[index]);
+            this.currentRoomChat.next(this.rooms.value[index]);
     }
 
     addMessage(msg: ChatMessage): void {
-        const index = this.findRoom(msg.roomId);
-        if (index !== undefined) {
-            if (this.rooms.value[index].id == this.currentRoom.value.id) {
-                const currentMessages = this.currentRoom.value.messages;
-                currentMessages.push(msg);
-                this.currentRoom.next({...this.rooms.value[index], messages: currentMessages});
-            } else {
+        if (msg.roomId== this.currentRoomChat.value.id) {
+            const currentMessages = this.currentRoomChat.value.messages;
+            currentMessages.push(msg);
+            this.currentRoomChat.next({...this.currentRoomChat.value, messages: currentMessages});
+        } else if (msg.roomId== this.currentGameRoom.value.id) {
+            const currentMessages = this.currentGameRoom.value.messages;
+            currentMessages.push(msg);
+            this.currentGameRoom.next({...this.currentGameRoom.value, messages: currentMessages});
+        } else {
+            const index = this.findRoom(msg.roomId);
+            if (index) {
                 const currentMessages = this.rooms.value[index].messages;
                 currentMessages.push(msg);
                 const newRooms = this.rooms.value;
                 newRooms[index] = {...newRooms[index], messages: currentMessages};
                 this.rooms.next(newRooms);
-                //this.rooms.value[index].next({...this.rooms.value[index], messages: currentMessages});
             }
         }
     }
@@ -96,19 +104,22 @@ export class RoomService {
     }
 
     addUser(roomId: string, user: User): void {
-        const rooms = this.rooms.value;
-        /*for (let i = 0; i < this.rooms.value.length; i++) {
-            if (rooms[i].id == roomId) {
-                rooms[i].userIds = [...rooms[i].userIds, user.id];
-                this.rooms.next(rooms);
-            }
-        }*/
-        if (this.currentRoom.value.id == roomId) {
-            
-            const currentRoom = this.currentRoom.value;
+        if (roomId == this.currentRoomChat.value.id) {
+            const currentRoom = this.currentRoomChat.value;
             currentRoom.userIds = [...currentRoom.userIds, user.id]
-            this.currentRoom.next(currentRoom);
-            console.log(this.currentRoom.value);
+            this.currentRoomChat.next(currentRoom);
+        } else if (roomId == this.currentGameRoom.value.id) {
+            const currentGameRoom = this.currentGameRoom.value;
+            currentGameRoom.userIds = [...currentGameRoom.userIds, user.id]
+            this.currentGameRoom.next(currentGameRoom);
+        } else {
+            const rooms = this.rooms.value;
+            for (let i = 0; i < this.rooms.value.length; i++) {
+                if (rooms[i].id == roomId) {
+                    rooms[i].userIds = [...rooms[i].userIds, user.id];
+                    this.rooms.next(rooms);
+                }
+            }
         }
     }
 }
