bcedf90dcf33f0e4c0da6e2c95ffbd74fa3313a1
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-15 22:36:23 -0400

    Début sidebar

--- clientLourd/electron-app/src/app/components/sidebar/sidebar.component.ts
+++ clientLourd/electron-app/src/app/components/sidebar/sidebar.component.ts
@@ -2,7 +2,12 @@ import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { MatSidenav } from "@angular/material/sidenav";
 import { MatSlideToggle } from "@angular/material/slide-toggle";
+import { RoomService } from "@app/services/room/room.service";
 import { ThemeService } from "@app/services/theme/theme.service";
+import { UserService } from "@app/services/user/user.service";
+import { WebSocketService } from "@app/services/web-socket/web-socket.service";
+import { User } from "@app/utils/interfaces/user";
+import { BehaviorSubject } from "rxjs";
 //import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
@@ -12,14 +17,37 @@ import { ThemeService } from "@app/services/theme/theme.service";
 })
 export class SidebarComponent {
     @Input() sidenavHandle!: MatSidenav;
-  public title = 'Fleet Management';
   private darkThemeIcon = 'nightlight_round';
   private lightThemeIcon = 'wb_sunny';
-  public lightDarkToggleIcon = this.darkThemeIcon;
+  public lightDarkToggleIcon = this.lightThemeIcon;
 
-  constructor(private themeService: ThemeService) { }
+  constructor(private userService: UserService, private socketService: WebSocketService, private roomService: RoomService, private themeService: ThemeService) {
+    this.user = this.userService.subjectUser;
+    document.getElementById("avatar")?.setAttribute("src", this.user.value.avatar.url);
+  }
 
   public doToggleLightDark() {
     this.themeService.switchValue();
+    if (this.lightDarkToggleIcon == this.darkThemeIcon) {
+        this.lightDarkToggleIcon = this.lightThemeIcon;
+    } else {
+        this.lightDarkToggleIcon = this.darkThemeIcon;
+    }
+  }
+
+  readonly title: string = "Scrabble";
+  isJoining = false;
+  public user: BehaviorSubject<User>;
+
+  isConnected(): boolean {
+    return this.userService.isLoggedIn;
+  }
+
+  logout(): void {
+    this.socketService.disconnect();
+  }
+
+  isInGame(): boolean {
+    return this.roomService.currentGameRoom.value.id != "";
   }
 }
