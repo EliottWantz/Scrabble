4c84a7631a0850fb5d35928e51d2e509caffaacc
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-03-27 17:07:42 -0400

    Modification join game component

--- clientLourd/electron-app/src/app/components/join-game/join-game.component.ts
+++ clientLourd/electron-app/src/app/components/join-game/join-game.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { MatDialogRef } from "@angular/material/dialog";
+import { MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material/dialog";
 import { GameService } from "@app/services/game/game.service";
 import { StorageService } from "@app/services/storage/storage.service";
 import { WebSocketService } from "@app/services/web-socket/web-socket.service";
@@ -7,6 +7,7 @@ import { ClientEvent } from "@app/utils/events/client-events";
 import { Game } from "@app/utils/interfaces/game/game";
 import { JoinGamePayload } from "@app/utils/interfaces/packet";
 import { BehaviorSubject } from "rxjs";
+import { JoinPrivateGameComponent } from "@app/components/join-private-game/join-private-game.component";
 
 @Component({
     selector: "app-join-game",
@@ -15,7 +16,7 @@ import { BehaviorSubject } from "rxjs";
 })
 export class JoinGameComponent {
     games: BehaviorSubject<Game[]>;
-    constructor(public dialogRef: MatDialogRef<JoinGameComponent>, private gameService: GameService, private webSocketService: WebSocketService, private storageService: StorageService) {
+    constructor(public dialogRef: MatDialogRef<JoinGameComponent>, public dialog: MatDialog, private gameService: GameService, private webSocketService: WebSocketService, private storageService: StorageService) {
         this.games = this.gameService.joinableGames;
     }
 
@@ -30,14 +31,30 @@ export class JoinGameComponent {
         return names;
     }
 
-    joinGame(gameId: string, password: string): void {
+    openDialogJoinPrivateGame(game: Game): void {
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.disableClose = true;
+        this.dialog.open(JoinPrivateGameComponent, {width: '75%',
+            minHeight: '70vh',
+            height : '50vh',
+            data: {game: game}
+        });
+    }
+
+    joinGame(game: Game): void {
         //this.stepper.selectedIndex = STEPPER_PAGE_IDX.confirmationPage;
-        const payload: JoinGamePayload = {
-            gameId: gameId,
-            password: password
-          }
-          const event : ClientEvent = "join-game";
-          this.webSocketService.send(event, payload);
+        if (game.isProtected) {
+            this.openDialogJoinPrivateGame(game);
+            this.close();
+        } else {
+            const payload: JoinGamePayload = {
+                gameId: game.id,
+                password: ""
+            }
+            const event : ClientEvent = "join-game";
+            this.webSocketService.send(event, payload);
+            this.close();
+        }
     }
 
     close() {
