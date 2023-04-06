4c186cff22163df978e8edf2ba11611b0fda76dc
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-04-06 14:01:51 -0400

    Modification de board

--- clientLourd/electron-app/src/app/components/board/board.component.ts
+++ clientLourd/electron-app/src/app/components/board/board.component.ts
@@ -10,6 +10,8 @@ import { Game, ScrabbleGame } from '@app/utils/interfaces/game/game';
 import { GameService } from '@app/services/game/game.service';
 import { MouseService } from '@app/services/mouse/mouse.service';
 import { MoveService } from '@app/services/game/move.service';
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
+import { Tile } from '@app/utils/interfaces/game/tile';
 
 @Component({
   selector: 'app-board',
@@ -24,7 +26,7 @@ export class BoardComponent implements OnInit {
 
   ngOnInit(): void {
     this.game.subscribe(() => {
-      console.log('game updated');
+      //console.log('game updated');
     });
   }
 
@@ -38,7 +40,7 @@ export class BoardComponent implements OnInit {
       this.moveService.selectedTiles.length == 1
     ) {
       this.mouseService.place(currentElem.nativeElement, row, col);
-      multiElem.nativeElement.remove();
+      //multiElem.nativeElement.remove();
     }
   }
 
@@ -53,5 +55,30 @@ export class BoardComponent implements OnInit {
       return 'TL';
     }
     return '';
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+    console.log(event.dropPoint);
+    console.log(document.elementFromPoint(event.dropPoint.x,event.dropPoint.y));
+    console.log(event.previousContainer);
+    let bruh = document.elementFromPoint(event.dropPoint.x, event.dropPoint.y);
+    if (document.getElementById("board")?.contains(bruh) == false) {
+        return;
+    }
+    if (bruh && bruh?.tagName === "DIV") {
+        bruh = bruh.parentElement;
+    }
+    const x = Number(bruh?.getAttribute("data-x"));
+    const y = Number(bruh?.getAttribute("data-y"));
+    const elem = event.item.element.nativeElement;
+    const tile : Tile = {letter: Number(elem.getAttribute("data-letter")), value: Number(elem.getAttribute("data-value"))};
+    if (this.gameService.scrabbleGame.value && event.previousContainer.element.nativeElement.dataset['x'] && event.previousContainer.element.nativeElement.dataset['y']) {
+      const newBoard = this.gameService.scrabbleGame.value.board;
+      console.log("deleting");
+      newBoard[parseInt(event.previousContainer.element.nativeElement.dataset['x'])][parseInt(event.previousContainer.element.nativeElement.dataset['y'])].tile = undefined;
+      this.gameService.scrabbleGame.next({...this.gameService.scrabbleGame.value, board: newBoard});
+    }
+    this.mouseService.place_drag_drop(elem, x, y, tile);
   }
 }
