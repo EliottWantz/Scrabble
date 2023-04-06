a37d9bc634fd89558ec4a4d5160cf3b7ab2ad62d
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-04-06 14:02:22 -0400

    Modification du rack

--- clientLourd/electron-app/src/app/components/rack/rack.component.ts
+++ clientLourd/electron-app/src/app/components/rack/rack.component.ts
@@ -4,9 +4,10 @@ import { Game, ScrabbleGame } from "@app/utils/interfaces/game/game";
 import { GameService } from "@app/services/game/game.service";
 import { UserService } from "@app/services/user/user.service";
 import { Tile } from "@app/utils/interfaces/game/tile";
-import {CdkDragDrop} from '@angular/cdk/drag-drop'
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
 import { MouseService } from "@app/services/mouse/mouse.service";
 import { TileComponent } from "../tile/tile.component";
+import { MoveService } from "@app/services/game/move.service";
 
 @Component({
     selector: "app-rack",
@@ -16,31 +17,36 @@ import { TileComponent } from "../tile/tile.component";
 export class RackComponent implements OnInit {
     game!: BehaviorSubject<ScrabbleGame | undefined>;
     rack: Tile[] = [];
-    constructor(private gameService: GameService, private userService: UserService, private mouseService:MouseService) {
+    constructor(private gameService: GameService, private userService: UserService, private mouseService:MouseService,
+        private moveService: MoveService) {
         this.game = this.gameService.scrabbleGame;
         const currentRack = this.getPlayerRack();
         if (currentRack)    
             this.rack = currentRack;
-        console.log(this.rack);
-        console.log(this.userService.subjectUser.value.id);
+        //console.log(this.rack);
+        //console.log(this.userService.subjectUser.value.id);
     }
 
     ngOnInit(): void {
         this.game.subscribe(() => {
-            console.log("game updated");
+            //console.log("game updated");
             const currentRack = this.getPlayerRack();
             if (currentRack)    
                 this.rack = currentRack;
         });
     }
 
+    checkIfTurn(): boolean {
+        return this.game.value?.turn === this.userService.currentUserValue.id;
+    }
+
     private getPlayerRack(): Tile[] | undefined {
-        console.log(this.game.value);
-        console.log(this.game.value?.players);
+        //console.log(this.game.value);
+        //console.log(this.game.value?.players);
         if (this.game.value?.players) {
             for (let i = 0; i < this.game.value.players.length; i++) {
                 if (this.game.value.players[i].id == this.userService.subjectUser.value.id) {
-                    console.log(this.game.value.players[i].rack);
+                    //console.log(this.game.value.players[i].rack);
                     return this.game.value.players[i].rack.tiles;
                 }   
             }
@@ -52,6 +58,7 @@ export class RackComponent implements OnInit {
     drop(event: CdkDragDrop<string[]>) {
         console.log(event);
         console.log(event.dropPoint);
+        console.log(event.previousContainer);
         console.log(document.elementFromPoint(event.dropPoint.x,event.dropPoint.y));
         let bruh = document.elementFromPoint(event.dropPoint.x, event.dropPoint.y);
         if (document.getElementById("board")?.contains(bruh) == false) {
@@ -65,6 +72,21 @@ export class RackComponent implements OnInit {
         const elem = event.item.element.nativeElement;
         const tile : Tile = {letter: Number(elem.getAttribute("data-letter")), value: Number(elem.getAttribute("data-value"))};
         console.log(tile);
+        let deleted = false;
+        for (let i = 0; i < this.rack.length; i++) {
+            if (this.rack[i].letter == tile.letter && !deleted) {
+                this.rack.splice(i, 1);
+                deleted = true;
+            }
+        }
+        /*const index = this.moveService.placedTiles.indexOf(tile, 0);
+        if (index > -1) {
+            const newBoard = this.gameService.scrabbleGame.value?.board;
+            if (this.gameService.scrabbleGame.value && newBoard) {
+                newBoard[Number(elem.getAttribute("data-y"))][Number(elem.getAttribute("data-x"))].tile = undefined;
+                this.gameService.scrabbleGame.next({...this.gameService.scrabbleGame.value, board: newBoard});
+            }
+        }*/
         this.mouseService.place_drag_drop(elem, x, y, tile);
       }
 }
