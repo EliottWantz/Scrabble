c81033e43b2f38ea7eb67b271a304fdacc4e481e
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-04-07 13:14:21 -0400

    Ajout du score pour indice

--- clientLourd/electron-app/src/app/utils/interfaces/game/move.d.ts
+++ clientLourd/electron-app/src/app/utils/interfaces/game/move.d.ts
@@ -2,6 +2,7 @@ export interface MoveInfo {
     type: "playTile" | "exchange" | "pass";
     letters?: string;
     covers?: Cover;
+    score?: number;
 }
 
 export type Cover = Record<string, string>;
