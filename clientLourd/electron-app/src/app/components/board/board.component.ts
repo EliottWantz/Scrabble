f394ebd0a5c9251e295e965a190755ca77abef1d
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-04-08 17:51:05 -0400

    Modif du board

--- clientLourd/electron-app/src/app/components/board/board.component.ts
+++ clientLourd/electron-app/src/app/components/board/board.component.ts
@@ -32,8 +32,10 @@ export class BoardComponent implements OnInit {
       private userService: UserService, private viewContainerRef: ViewContainerRef, private eRef: ElementRef) {
         this.game = this.gameService.scrabbleGame;
         this.gameService.dragging.subscribe((val) => {
-          if (val && this.direction)
+          if (val && this.direction) {
             this.direction.destroy();
+            this.direction = undefined;
+          }
         });
     }
 
@@ -43,67 +45,86 @@ export class BoardComponent implements OnInit {
     });
   }
 
+  checkIfNewTile(tile: Tile | undefined): boolean {
+    if (tile) {
+      if (tile.letter === undefined) {
+        return false;
+      }
+      if (tile.disabled === undefined) {
+        return false;
+      } else {
+        return tile.disabled === false;
+      }
+    }
+    return false;
+  }
+
   @ViewChildren('elem', {read: ViewContainerRef}) elements!: QueryList<ViewContainerRef>;
   @ViewChildren('multis') multis!: QueryList<ElementRef>;
+
   clicked(row: number, col: number): void {
-    if (this.gameService.dragging.value === true) {
-      console.log("????");
-      this.gameService.resetSelectedAndPlaced();
-      console.log(this.gameService.scrabbleGame.value);
-      console.log(this.gameService.oldGame);
-    }
     if (!this.gameService.scrabbleGame.value?.board[row][col].tile?.letter && this.userService.currentUserValue.id == this.gameService.scrabbleGame.value?.turn) {
+      if (this.gameService.dragging.value === true) {
+        //this.gameService.resetSelectedAndPlaced();
+        this.gameService.resetSelectedAndPlaced();
+        //if (newGame) this.gameService.scrabbleGame.next(newGame);
+      }
       this.gameService.dragging.next(false);
-      console.log(this.elements.toArray()[row * 15 + col].element.nativeElement.children);
-      let clicked = this.elements.toArray()[row * 15 + col].element.nativeElement;
+      if (this.direction) {
+        if (this.direction.instance.initialX !== col || this.direction.instance.initialY !== row) {
+          this.direction.destroy();
+          this.direction = undefined;
+          this.gameService.resetSelectedAndPlaced();
+          //if (newGame) this.gameService.scrabbleGame.next(newGame);
+        } else {
+          this.direction.instance.clicked();
+          return;
+        }
+      }
+      setTimeout(() => {
+        let clicked = this.elements.toArray()[row * 15 + col].element.nativeElement;
       while (clicked && clicked?.tagName !== "MAT-GRID-TILE") {
         clicked = clicked.parentElement;
       }
-      console.log(clicked);
       if (clicked.tagName === "MAT-GRID-TILE") {
-        if (!this.direction || this.direction.instance.x !== col || this.direction.instance.y !== row) {
-          if (this.direction) {
-            this.direction.destroy();
-            this.gameService.resetSelectedAndPlaced();
-          }
-          console.log("created direction");
           this.direction = this.elements.toArray()[row * 15 + col].createComponent(DirectionComponent);
           this.direction.instance.x = col;
           this.direction.instance.y = row;
           this.direction.instance.initialY = row;
           this.direction.instance.initialX = col;
           clicked.appendChild(this.direction.location.nativeElement);
-          /*const multiElem = this.multis.toArray()[row * 15 + col];
-          multiElem.nativeElement.remove();*/
-        } else {
-          console.log("switching");
-          this.direction.instance.clicked();
-        }
       }
+      }, 100);
     }
   }
 
   @HostListener("document:click", ['$event'])
   clickout(event: any) {
-    console.log(event);
     const elem = document.elementFromPoint(event.x,event.y);
-    console.log(elem);
     if (!document.getElementById("board")?.contains(elem) && this.direction) {
       this.direction.destroy();
+      this.direction = undefined;
       this.gameService.resetSelectedAndPlaced();
+      //if (newGame) this.gameService.scrabbleGame.next(newGame);
     }
   }
 
   @HostListener("document:keyup", ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (this.direction) {
+    if (event.key === "Enter" && this.gameService.placedTiles > 0) {
+      this.moveService.playTiles();
+    } else if (this.direction) {
       if (event.key === "Backspace") {
         this.removeLastLetter();
-      } else if (event.key === "Enter") {
-        this.moveService.playTiles();
       } else {
         const key = event.key.charCodeAt(0);
-        this.checkKey(key < 97, key);
+        if (key > 64 && key < 91) {
+          // Maj
+          this.checkKey(true, key);
+        } else if (key > 96 && key < 123) {
+          // Normal
+          this.checkKey(false, key);
+        }
       }
     }
   }
@@ -132,11 +153,7 @@ export class BoardComponent implements OnInit {
       for (let i = 0; i < newPlayers.length; i++) {
         if (newPlayers[i].id == this.userService.currentUserValue.id) {
           if (newBoard[y][x].tile) {
-            console.log(newBoard[y][x])
             const oldTile = newBoard[y][x].tile as Tile;
-            console.log(oldTile);
-            console.log(newBoard);
-            console.log(this.gameService.scrabbleGame.value.board);
             if (oldTile.letter >= 97) {
               newPlayers[i].rack.tiles.push({letter: oldTile.letter, value: oldTile.value, disabled: false});
             } else {
@@ -205,70 +222,62 @@ export class BoardComponent implements OnInit {
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    console.log(event);
-    console.log(event.dropPoint);
-    console.log(document.elementFromPoint(event.dropPoint.x,event.dropPoint.y));
-    console.log(event.previousContainer);
     this.gameService.selectedTiles = [];
     this.mouseService.resetColor();
     let bruh = document.elementFromPoint(event.dropPoint.x, event.dropPoint.y);
     if (document.getElementById("board")?.contains(bruh) == false && document.getElementById("rack")?.contains(bruh) == false) {
         return;
     }
-    while (bruh && bruh?.tagName !== "MAT-GRID-TILE") {
-      bruh = bruh.parentElement;
+    if (this.gameService.dragging.value === false) {
+      this.gameService.resetSelectedAndPlaced();
+      //if (newGame) this.gameService.scrabbleGame.next(newGame);
     }
+    setTimeout(() => {
+      this.gameService.dragging.next(true);
     if (bruh?.classList.contains("bad")) {
-      console.log("bad");
-      return;
-    }
-    const x = Number(bruh?.getAttribute("data-x"));
-    const y = Number(bruh?.getAttribute("data-y"));
-    if (this.gameService.scrabbleGame.value?.board[x][y].tile?.letter) {
       return;
     }
     const elem = event.item.element.nativeElement;
+    console.log(bruh);
     const tile : Tile = {letter: Number(elem.getAttribute("data-letter")), value: Number(elem.getAttribute("data-value"))};
-    if (document.getElementById("board")?.contains(bruh) == true) {
-      this.mouseService.place_drag_drop(elem, x, y, tile);
-    }
     const oldX = event.previousContainer.element.nativeElement.dataset['x'];
     const oldY = event.previousContainer.element.nativeElement.dataset['y'];
-    if (this.gameService.scrabbleGame.value && oldX && oldY) {
-      const newBoard = this.gameService.scrabbleGame.value.board;
-      /*if (this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile) {
-        console.log(this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile);
-        console.log(this.gameService.placedTiles);
-        let deleted = false;
-        for (let i = 0; i < this.gameService.placedTiles.length; i++) {
-          if (!deleted && (this.gameService.placedTiles[i] == this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile ||
-            (this.gameService.placedTiles[i].letter == this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile?.letter &&
-            this.gameService.placedTiles[i].value == this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile?.value && 
-            this.gameService.placedTiles[i].x == this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile?.x &&
-            this.gameService.placedTiles[i].y == this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile?.y))) {
-              console.log("deleting placed");
-              console.log(this.gameService.placedTiles);
-            const newplaced = this.gameService.placedTiles.splice(i, 1);
-            this.gameService.placedTiles = newplaced;
-            console.log(newplaced);
-            console.log(this.gameService.placedTiles);
-            deleted = true;
-          }
-        }
-      }*/
-      console.log("deleting");
-      newBoard[parseInt(oldX)][parseInt(oldY)].tile = undefined;
-      this.gameService.scrabbleGame.next({...this.gameService.scrabbleGame.value, board: newBoard});
-    }
-    if (document.getElementById("rack")?.contains(bruh) == true && this.gameService.scrabbleGame.value) {
+    if (document.getElementById("board")?.contains(bruh) == true) {
+      while (bruh && bruh?.tagName !== "MAT-GRID-TILE") {
+        bruh = bruh.parentElement;
+      }
+      const x = Number(bruh?.getAttribute("data-x"));
+      const y = Number(bruh?.getAttribute("data-y"));
+      console.log(bruh);
+      if (this.gameService.scrabbleGame.value?.board[x][y].tile?.letter) {
+        console.log("has letter");
+        return;
+      }
+      if (this.gameService.scrabbleGame.value && oldX && oldY) {
+        const newBoard = this.gameService.scrabbleGame.value.board;
+        newBoard[parseInt(oldX)][parseInt(oldY)].tile = undefined;
+        this.gameService.placedTiles--;
+      }
+      this.mouseService.place_drag_drop(elem, x, y, tile);
+    } else if (document.getElementById("rack")?.contains(bruh) == true && this.gameService.scrabbleGame.value  && oldX && oldY) {
+      console.log("rack");
       if (this.gameService.scrabbleGame.value) {
         const players: Player[] = this.gameService.scrabbleGame.value.players;
         for (let i = 0; i < players.length; i++) {
           if (players[i].id == this.userService.currentUserValue.id) {
-            players[i].rack.tiles.push({letter: tile.letter, value: tile.value, disabled: false});
+            if (this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile) {
+              players[i].rack.tiles.push(this.gameService.scrabbleGame.value.board[parseInt(oldX)][parseInt(oldY)].tile as Tile);
+              const newBoard = this.gameService.scrabbleGame.value.board;
+              newBoard[parseInt(oldX)][parseInt(oldY)].tile = undefined;
+              this.gameService.placedTiles--;
+              this.gameService.scrabbleGame.next({...this.gameService.scrabbleGame.value, board: newBoard, players: players});
+              return;
+            }
           }
         }
       }
     }
+    }, 100);
+    
   }
 }
