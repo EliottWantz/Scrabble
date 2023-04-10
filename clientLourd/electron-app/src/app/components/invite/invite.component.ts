96061270e6ad1a8697d6fe4774472276c6c55343
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2023-04-10 16:27:52 -0400

    Ajout langue dans html

--- clientLourd/electron-app/src/app/components/invite/invite.component.ts
+++ clientLourd/electron-app/src/app/components/invite/invite.component.ts
@@ -1,14 +1,11 @@
-import { Component, ElementRef, QueryList, ViewChildren } from "@angular/core";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { Router } from "@angular/router";
+import { Component } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { CommunicationService } from "@app/services/communication/communication.service";
 import { GameService } from "@app/services/game/game.service";
 import { InviteService } from "@app/services/invite/invite.service";
 import { StorageService } from "@app/services/storage/storage.service";
 import { UserService } from "@app/services/user/user.service";
-import { WebSocketService } from "@app/services/web-socket/web-socket.service";
 import { Game } from "@app/utils/interfaces/game/game";
-import { LeaveGamePayload } from "@app/utils/interfaces/packet";
 import { JoinPrivateGameComponent } from "@app/components/join-private-game/join-private-game.component";
 
 @Component({
@@ -19,7 +16,7 @@ import { JoinPrivateGameComponent } from "@app/components/join-private-game/join
 export class InviteComponent {
     invites: {inviterId: string, game: Game, error: string, password: string | undefined}[] = [];
     constructor(private storageService: StorageService, private commService: CommunicationService, private userService: UserService, private inviteService: InviteService,
-        private gameService: GameService, private webSocketService: WebSocketService, private router: Router, public dialog: MatDialog) {
+        public dialog: MatDialog) {
         this.inviteService.invites.subscribe((invites) => {
             this.invites = invites;
         });
@@ -48,27 +45,20 @@ export class InviteComponent {
 
     acceptInvite(index: number): void {
         if (this.invites[index].game.isProtected && this.invites[index].password === "") {
-            this.invites[index].error = "password required";
+            this.invites[index].error = "password-required";
             return;
-        }
-        if (this.gameService.game.value !== undefined) {
-            const payload: LeaveGamePayload = {
-                gameId: this.gameService.game.value.id
-            } 
-            this.webSocketService.send("leave-game", payload);
-            this.router.navigate(["/home"]);
         }
         let currentPassword = this.invites[index].password;
         if (currentPassword === undefined) {
             currentPassword = "";
         }
         this.commService.acceptGameInvite(this.userService.currentUserValue.id, this.invites[index].inviterId, this.invites[index].game.id, currentPassword).then((res) => {
-            //const newInvites = this.invites.splice(index, 1);
-            //this.inviteService.invites.next(newInvites);
             console.log(res);
         }).catch((err) => {
             if (err.error.message == "password mismatch") {
-                this.invites[index].error = "password missmatch";
+                this.invites[index].error = "password-mismatch";
+            } else if (err.error.message == "game not found") {
+                this.invites[index].error = "game-not-found";
             } else {
                 if (this.invites[index].game.isPrivateGame) {
                     this.openDialogJoinPrivateGame(this.invites[index].game);
@@ -80,14 +70,7 @@ export class InviteComponent {
     }
 
     denyInvite(index: number): void {
-        //const newInvites = this.invites.splice(index, 1);
-        //this.inviteService.invites.next(newInvites);
-        this.invites.splice(index, 1);
-        let currentPassword = this.invites[index].password;
-        if (currentPassword === undefined) {
-            currentPassword = "";
-        }
-        this.commService.declineGameInvite(this.invites[index].inviterId, this.userService.currentUserValue.id, this.invites[index].game.id, currentPassword).then((res) => {
+        this.commService.declineGameInvite(this.invites[index].inviterId, this.userService.currentUserValue.id, this.invites[index].game.id, "").then((res) => {
             console.log(res);
         }).catch((err) => {
             this.invites.splice(index, 1);
