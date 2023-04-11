74b75014bcfa6b5bcdef21b17de9fb211c47a66b
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2023-04-11 19:44:53 -0400

    same fix other route

--- server/pkg/api/ws/controller.go
+++ server/pkg/api/ws/controller.go
@@ -209,6 +209,12 @@ func (m *Manager) GetFriendById(c *fiber.Ctx) error {
 func (m *Manager) RemoveFriend(c *fiber.Ctx) error {
 	id := c.Params("id")
 	friendId := c.Params("friendId")
+	if id == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "id is required")
+	}
+	if friendId == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "friendId is required")
+	}
 
 	err := m.RemoveFriendFromList(id, friendId)
 	if err != nil {
